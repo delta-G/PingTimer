3cd67ede0a6065330165e5b086c5a38d05b6a78e
David <6957239+delta-G@users.noreply.github.com>
2023-10-28 16:03:34 -0500

    simplified and commented example

--- PingTImerExample/PingTImerExample.ino
+++ PingTImerExample/PingTImerExample.ino
@@ -1,26 +1,32 @@
+/*
+*   PingTimerExample.ino
+*   Copyright (C) 2019 2023 David C.(deltagrobotics@gmail.com)
+*
+*   The Trigger and Echo pins must be connected as described
+*        in the library documentation
+*
+*/
 
+
+// Include the library
 #include "PingTimer.h"
 
-int distance;
-
-void setup() {
-  
+void setup() {  
   Serial.begin(9600);
+  // call ping.begin() to set things up
   ping.begin();
+  // call sendPing to start a measurement. 
   ping.sendPing();
-
 }
 
 void loop() {
-
   //  if the ping sensor has new data save it to distance and print it.
   if(ping.hasNewData()){
-    distance = ping.getDistanceMM();
+    // get distance in millimeters
+    int distance = ping.getDistanceMM();
     Serial.print("Current Distance: ");
     Serial.println(distance);
+    // call sendPing to start a new measurement
     ping.sendPing();   
   }   
-
-  //  Do other things not blocked by ping sensor
-
 }
